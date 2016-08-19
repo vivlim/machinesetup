db90142496203a2b51c685fba3c31ca8a11593d8
Mike Lim <mike@mjlim.net>
2016-08-19 00:48:12 -0700

    add missing thing to emacs setup script

--- packageScripts/emacs64-onInstall.ps1
+++ packageScripts/emacs64-onInstall.ps1
@@ -31,3 +31,13 @@ if (Test-Path $serverpath)
     $acl.SetOwner($user.User)
     Set-Acl -Path $serverpath -AclObject $acl
 }
+
+if (!$env:HOME) # emacs looks here to pull in the spacemacs config.
+{
+    echo "Setting HOME environment variable to $env:USERPROFILE"
+    [Environment]::SetEnvironmentVariable("HOME", $env:USERPROFILE, "Machine")
+}
+else
+{
+    echo "HOME environment variable already exists and is $env:HOME"
+}
