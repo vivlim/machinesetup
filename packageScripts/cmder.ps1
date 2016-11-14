ec66d1cc9e72f44ef3a85787fd3e4881a25938ae
Vivian Lim <vivvnlim@gmail.com>
2016-11-13 23:24:17 -0800

    Make cmder update script patch the local machine's defined tasks into the conemu.xml updating from git

--- packageScripts/cmder.ps1
+++ packageScripts/cmder.ps1
@@ -1,10 +1,39 @@
 $conemuDir = "C:\tools\cmder\vendor\conemu-maximus5\"
-echo "Copying conemu config from git to $conemuDir"
-pushd $conemuDir
-$date = get-date -Format "yyyyMMdd_h-m-s"
-if (Test-Path "$conemuDir\ConEmu.xml")
+$localScriptPath = "$conemuDir\ConEmu.xml"
+$gitScriptPath = "$PSScriptRoot\cmder\ConEmu.xml"
+
+function Patch-ConEmuConfig()
 {
-    mv ConEmu.xml "ConEmu-$date.xml"
+    $date = get-date -Format "yyyyMMdd_h-m-s"
+    $backupLocalScriptPath = "$PSScriptRoot\cmder\ConEmu-$date.xml"
+    mv $localScriptPath $backupLocalScriptPath
+    cp $gitScriptPath $localScriptPath
+
+    [xml]$oldLocalConfig = Get-Content -Path $backupLocalScriptPath
+    [xml]$newLocalConfig = Get-Content -Path $localScriptPath
+
+    $oldTasksUnimported = (Select-Xml -Xml $oldLocalConfig -XPath "/key[@name='Software']/key[@name='ConEmu']/key[@name='.Vanilla']/key[@name='Tasks']").Node
+    if ($oldTasksUnimported -eq $null)
+    {
+      echo "Local ConEmu.xml is missing Tasks, using version from git instead."
+      return
+    }
+
+    $oldTasks = $newLocalConfig.ImportNode($oldTasksUnimported, $true)
+    $newTasks = (Select-Xml -Xml $newLocalConfig -XPath "/key[@name='Software']/key[@name='ConEmu']/key[@name='.Vanilla']/key[@name='Tasks']").Node
+    $newTasksParent = (Select-Xml -Xml $newLocalConfig -XPath "/key[@name='Software']/key[@name='ConEmu']/key[@name='.Vanilla']").Node
+
+    $newTasksParent.ReplaceChild($oldTasks, $newTasks) # copy oldtasks into newconfig
+    $newLocalConfig.Save($localScriptPath) # write to file
 }
-cp "$PSScriptRoot\cmder\ConEmu.xml" .
-popd
+
+if (Test-Path $localScriptPath)
+{
+    echo "Patching conemu config with changes from git, excluding any tasks defined on this machine. $localScriptPath"
+    Patch-ConEmuConfig
+}
+else
+{
+    echo "No conemu config currently. Copying conemu config from git to $conemuDir"
+    cp $gitScriptPath $localScriptPath
+}
