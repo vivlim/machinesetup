26608125112572dbcd4824bdb36e6d4eb86a5c7d
Vivian Lim <vivvnlim@gmail.com>
2016-11-13 22:44:09 -0800

    Vim config - fully update plugins every time instead of only on install

--- packageScripts/vim.ps1
+++ packageScripts/vim.ps1
@@ -1,1 +1,37 @@
 [Environment]::SetEnvironmentVariable("GIT_EDITOR", "vim", "Machine")
+
+refreshenv
+
+function Update-Vim-Plugins()
+{
+    git submodule init
+    git submodule update
+    vim +PluginInstall +qall
+}
+
+echo "Setting up vim config."
+
+if (Test-path $env:USERPROFILE/vimfiles/)
+{
+    echo "vimfiles exists. updating it..."
+    pushd $env:USERPROFILE/vimfiles
+    git pull origin master
+
+    Update-Vim-Plugins
+
+    popd
+}
+else
+{
+    echo "vimfiles doesn't exist. cloning from my github"
+    pushd $env:USERPROFILE
+    git clone git@github.com:vivvnlim/vimfiles.git
+    if (!(Test-path $env:USERPROFILE/vimfiles/))
+    {
+        echo "cloning failed. falling back to https clone"
+        git clone https://github.com/vivvnlim/vimfiles.git
+    }
+    Update-Vim-Plugins
+
+    popd
+}
