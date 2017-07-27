5326c616b1418b059a0b10e830453e312bf1bb01
Vivian Lim <vivvnlim@gmail.com>
2017-07-27 14:03:55 -0700

    Add neovim package setup script which was missing

---
+++ packageScripts/neovim.ps1
@@ -0,0 +1,47 @@
+﻿refreshenv
+
+function Update-Vim-Plugins()
+{
+    git submodule init
+    git submodule update
+    nvim +PluginInstall +qall
+}
+
+echo "Setting up vim config."
+
+if (Test-path $env:USERPROFILE/AppData/Local/nvim/)
+{
+    echo "vimfiles exists. updating it..."
+    pushd $env:USERPROFILE/AppData/Local/nvim
+    git pull origin master
+
+    Update-Vim-Plugins
+
+    popd
+}
+else
+{
+    echo "vimfiles doesn't exist. cloning from my github"
+    pushd $env:USERPROFILE/AppData/Local/nvim
+    git clone git@github.com:vivvnlim/vimfiles.git nvim
+    git checkout master-neovim
+    if (!(Test-path $env:USERPROFILE/AppData/Local/nvim/))
+    {
+        echo "cloning failed. falling back to https clone"
+        git clone https://github.com/vivvnlim/vimfiles.git nvim
+        git checkout master-neovim
+    }
+    Update-Vim-Plugins
+
+    popd
+}
+
+if (!(Get-Command "nvim" -ErrorAction SilentlyContinue))
+{
+    if (Test-Path "C:\tools\neovim\neovim\bin\nvim.exe"){
+        echo "Adding neovim to path"
+        [Environment]::SetEnvironmentVariable("Path", $Env:Path + ";C:\tools\neovim\neovim\bin\", "Machine")
+        refreshenv
+        $Env:Path = $Env:Path + ";C:\tools\neovim\neovim\bin\"
+    }
+}
