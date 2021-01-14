c1835701a407889b4dc967f8cfec63b34231b399
Vivian Lim <vivlim@microsoft.com>
2021-01-14 14:35:12 -0800

    Print the assembly's public key and use json so linebreaks aren't inserted.

--- Get-AssemblyVersion.ps1
+++ Get-AssemblyVersion.ps1
@@ -9,17 +9,20 @@ Get-ChildItem $assemblies |
     ForEach-Object {
         Write-Host $_.FullName
         try {
-            $_ | Add-Member NoteProperty FileVersion ($_.VersionInfo.FileVersion)
+            $_ | Add-Member NoteProperty FileVersion ($_.VersionInfo.FileVersion.ToString())
             $_ | Add-Member NoteProperty AssemblyVersion (
-                [Reflection.AssemblyName]::GetAssemblyName($_.FullName).Version
+                [Reflection.AssemblyName]::GetAssemblyName($_.FullName).Version.ToString()
             )
             $_ | Add-Member NoteProperty AssemblyFullName (
                 [Reflection.AssemblyName]::GetAssemblyName($_.FullName).FullName
             )
+            $_ | Add-Member NoteProperty AssemblyPublicKey (
+                ([Reflection.AssemblyName]::GetAssemblyName($_.FullName).GetPublicKey() | ForEach-Object ToString X2) -join ''
+            )
         } catch {}
         $_
     } |
-    Select-Object Name,FileVersion,AssemblyVersion,AssemblyFullName| Format-List
+    Select-Object Name,FullName,FileVersion,AssemblyVersion,AssemblyFullName,AssemblyPublicKey | ConvertTo-Json
 
 if ($wait) {
     Read-Host
